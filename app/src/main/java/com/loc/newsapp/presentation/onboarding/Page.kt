3519ecb8041d5603920871f69dff3d5ca6e95b07
lesediS <selekelesedi25@gmail.com>
2024-07-05 09:16:40 +0200

    fin

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
@@ -13,16 +13,16 @@ val pages = listOf(
     Page(
         title = "Latest news",
         description = "Get the latest scoops from across the globe.",
-        image = R.drawable.onboarding1
+        image = R.drawable.latest_news
     ),
     Page(
         title = "Always relevant",
         description = "Find news relevant to you in just a few clicks.",
-        image = R.drawable.onboarding2
+        image = R.drawable.onboarding1
     ),
     Page(
         title = "Save for later",
-        description = "Save articles you may want to read later.",
-        image = R.drawable.onboarding3
+        description = "Save articles you may want to read later wherever, whenever.",
+        image = R.drawable.international
     )
 )
