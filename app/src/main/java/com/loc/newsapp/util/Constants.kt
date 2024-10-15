cd79cefb27719056b7f2f0583a7fe4332017d4ee
Lesedi Seleke <selekelesedi25@gmail.com>
2024-10-15 13:32:48 +0200

    removed api key

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -4,9 +4,9 @@ object Constants {
 
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
-    const val API_KEY = "VxNiVjQkOcN1KlLG1SD2N9XjeAu7ZQAeE5WiLZlx"
+    const val API_KEY = ""
     const val BASE_URL = "https://api.thenewsapi.com/v1/news/top/"
-    /*const val API_KEY = "4a0cfb01740a4d3ead22d3b02078b816"
+    /*const val API_KEY = ""
     const val BASE_URL = "https://newsapi.org/v2/"*/
 
     const val NEWS_DATABASE_NAME = "news_dp"
